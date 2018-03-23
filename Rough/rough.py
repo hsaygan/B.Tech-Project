ad367b74aab44bff30e40b721d41a6e727bd128f
hsaygan <realyashnag@gmail.com>
2018-03-23 09:32:38 +0530

    Update

--- Rough/rough.py
+++ Rough/rough.py
@@ -1,6 +1,14 @@
-def process_random(file_path):
-    with open(file_path, buffering=3) as f:
-        for line in f:
-            print(line)
+import csv
 
-process_random("textfile.txt")
+output_file = "new_lexicon.pickle"
+
+def process_random(file_path, starting_line, ending_line, output_path):
+    data = 0
+    with open(file_path, 'r') as f:
+        entire_file = list(csv.reader(f))
+        for line in entire_file[starting_line:ending_line]:
+            line = list(line)
+            print (line)
+
+
+process_random("textfile.csv", 0, 2)
